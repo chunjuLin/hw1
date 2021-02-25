50f4c175588b574b5063b2f5dfc685d17b6c406c
chunjuLin <linchun0308@gmail.com>
2021-02-25 07:12:43 -0800

    add hw1.py

--- hw1.py
+++ hw1.py
@@ -8,7 +8,7 @@ import csv
 # Part. 2
 #=======================================
 # Read cwb weather data
-cwb_filename = 'sample_input.csv'
+cwb_filename = '107061234.csv'
 data = []
 header = []
 maximum = [-999,-999,-999,-999,-999]
